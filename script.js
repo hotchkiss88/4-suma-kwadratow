f88a2dfc7916f945c754d35a6fc0df6d4830dddd
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-15 11:44:12 +0000

    code formatting

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function mF(arr = [0, 1, 2, 3, 4, 5]) {
-let square = 0;
-    for (let item of arr) {
+function mF(arr) {
+  let square = 0;
 
-    square +=  Math.pow(item, 2);
-    }
-console.log(square);
+  for (let item of arr) {
+
+    square += Math.pow(item, 2);
+  }
+
+  console.log(square);
 }
 
-mF();
+mF([0, 1, 2, 3, 4, 5]);
+
