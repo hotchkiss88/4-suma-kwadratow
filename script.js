a5392d3e7eeb5bcf7275e9f3e3b04d9135f3acd2
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-15 11:50:38 +0000

    add return

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 function mF(arr) {
+
   let square = 0;
 
   for (let item of arr) {
@@ -6,8 +8,23 @@ function mF(arr) {
     square += Math.pow(item, 2);
   }
 
-  console.log(square);
+  return square
 }
 
-mF([0, 1, 2, 3, 4, 5]);
+console.log(mF([0, 1, 2, 3, 4, 5]));
+
+
+
+// function mF(arr) {
+//   let square = 0;
+
+//   for (let item of arr) {
+
+//     square += Math.pow(item, 2);
+//   }
+
+//   console.log(square);
+// }
+
+// mF([0, 1, 2, 3, 4, 5]);
 
